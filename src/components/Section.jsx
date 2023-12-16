f89bc9bc075046b8551bf839f1140c6b24b3bdfa
Lucas Gabriel Nuñez <65827279+Lucasnu-star@users.noreply.github.com>
2023-12-16 01:04:53 -0300

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -6,7 +6,7 @@ function Section() {
 	return (
 		<div className="flex flex-col w-full">
 			{dataMenues.map(menu => (
-				<div key={menu.id} className="text-2xl my-3 scroll-mt-16" id={menu.id}>
+				<div key={menu.id} className="text-2xl my-3 scroll-mt-20" id={menu.id}>
 					<h1 className=" w-fit mx-6">{menu.title}</h1>
 					<h3 className="text-sm text-gray-500 mb-5 px-6">{menu?.subTitle}</h3>
 					{menu?.items?.map((item, index) => (
